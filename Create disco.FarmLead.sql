24dedec11654a3f09821b7b9978a77b3964e8460
fbouteruche <francois.bouteruche.perso@live.fr>
2021-11-30 01:09:22 -0800

    Add the assets for the demo

---
+++ Create disco.FarmLead.sql
@@ -0,0 +1,34 @@
+﻿CREATE LOGIN webuser WITH PASSWORD = 'Disco.2021'
+GO
+
+CREATE USER webuser
+	FOR LOGIN webuser WITH DEFAULT_SCHEMA = disco
+GO
+
+ALTER LOGIN [webuser] WITH DEFAULT_DATABASE = [disco]
+
+ALTER ROLE [db_datawriter]
+	ADD MEMBER [webuser]
+GO
+
+ALTER ROLE [db_datareader]
+	ADD MEMBER [webuser]
+GO
+
+CREATE SCHEMA disco
+GO
+
+CREATE TABLE [disco].[FarmLeads]
+(
+	[Id] INT IDENTITY(1,1) PRIMARY KEY,
+	[FarmName] NVARCHAR(40) NOT NULL,
+	[ContactFirstName] NVARCHAR(20) NOT NULL, 
+	[ContactLastName] NVARCHAR(20) NOT NULL, 
+	[AddressLine1] NVARCHAR(20) NOT NULL, 
+	[AddressLine2] NVARCHAR(20),  
+	[ZipCode] NVARCHAR(20) NOT NULL,  
+	[City] NVARCHAR(20) NOT NULL, 
+	[Country] NVARCHAR(20) NOT NULL,
+	[Status] INT 
+)
+GO
---
+++ InstallationScript.ps1
@@ -0,0 +1,28 @@
+﻿Install-Module -Name AWS.Tools.Lightsail 
+
+#create a new windows lightsail instances
+New-LSInstance -InstanceName 'webserver1' -AvailabilityZone 'us-east-1a' -BlueprintId 'windows_server_2019' -BundleId 'medium_win_2_0' -UserData '
+<powershell>
+Install-WindowsFeature Web-Server
+Install-WindowsFeature Web-Mgmt-Tools
+Install-WindowsFeature Web-Asp-NET45
+</powershell>'
+
+New-LSInstance -InstanceName 'dbserver' -AvailabilityZone 'us-east-1a' -BlueprintId 'windows_server_2016_sql_2016_express' -BundleId 'medium_win_2_0'
+
+while((Get-LSInstanceState -InstanceName 'webserver1' -Select 'State.Code') -ne 16)
+{
+  Write-Host 'wait for instances to run'
+  Start-Sleep -Seconds 1
+}
+
+while((Get-LSInstanceState -InstanceName 'dbserver' -Select 'State.Code') -ne 16)
+{
+  Write-Host 'wait for instances to run'
+  Start-Sleep -Seconds 1
+}
+
+Write-Host 'instances are running'
+
+Get-LSInstance -InstanceName 'webserver1'
+Get-LSInstance -InstanceName 'dbserver'
